911a0544c6a224655c6aa35913106ad7d7fd68c1
shrish <mynameisshrish@gmail.com>
2024-04-12 16:21:24 +0530

    Day 4 Doctor App

---
+++ Day4/Doctor Console App/Doctor App/Doctor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doctor_App
+{
+    class Doctor
+    {
+        public int Id;
+        public string Name;
+        public int Age;
+        public int Exp;
+        public string Qualification;
+        public string Speciality;
+
+        /// <summary>
+        /// Constructor that initialises all the details of a Doctor.
+        /// </summary>
+        /// <param name="id">Integer ID</param>
+        /// <param name="name">Name in String</param>
+        /// <param name="age">Age in Int</param>
+        /// <param name="exp">Work Experience years in Int</param>
+        /// <param name="qualification">Qualification in String</param>
+        /// <param name="specality">Department of Specialisation in String</param>
+        public Doctor(int id,string name, int age, int exp, string qualification, string specality) 
+        {
+            Name = name;  
+            Id = id;
+            Age = age;
+            Exp = exp;
+            Qualification = qualification;
+            Speciality = specality;
+        }
+
+        /// <summary>
+        /// Displays all the details of the object in the console.
+        /// </summary>
+        public void displayDetails()
+        {
+            Console.WriteLine("ID: "+Id);
+            Console.WriteLine("Name: "+Name);
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("Experience: " + Exp);
+            Console.WriteLine("Qualification: " + Qualification);
+            Console.WriteLine("Speciality: " + Speciality);
+        }
+    }
+}
---
+++ Day4/Doctor Console App/Doctor App/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Doctor_App
+{
+    class Program
+    {
+        /// <summary>
+        /// Gets details from the console and creates a Doctor object.
+        /// </summary>
+        /// <param name="id">ID of the Doctor in INT</param>
+        /// <returns>A Doctor  object reference</returns>
+        static Doctor createDoctor(int id)
+        {
+            Console.Write("Enter Name: ");
+            string Name = Console.ReadLine();
+
+            Console.Write("Enter Age: ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Experience: ");
+            int Exp=Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter Qualification: ");
+            string Qualification=Console.ReadLine();
+            
+            Console.Write("Enter Speciality: ");
+            string Speciality=Console.ReadLine();
+            Console.WriteLine();
+            
+            return new Doctor(id, Name, Age, Exp, Qualification, Speciality); ;
+        }
+        static void Main(string[] args)
+        {
+         
+            Doctor[] doctors = new Doctor[3];
+            Console.WriteLine("Enter Details for Doctors:");
+            for(int i=0;i<doctors.Length;i++)
+            {
+                doctors[i]=createDoctor(100+i);
+            }
+
+            Console.WriteLine("Displaying Details: ");
+
+            for(int i=0;i<doctors.Length;i++) 
+            {
+                doctors[i].displayDetails();
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter a Speciality to display all the doctors in that field:");
+            string speciality=Console.ReadLine();
+
+            for(int i=0;i<doctors.Length;i++)
+            {
+                if (doctors[i].Speciality == speciality)
+                {
+                    Console.WriteLine("Doctor Details:");
+                    doctors[i].displayDetails();
+                    Console.WriteLine();    
+                }
+            }
+
+
+
+           
+            
+            
+        }
+    }
+}
